1e4e4c8e17480eaf291703a24e32fc9bce632c48
reinaldo-santosBa <reinaldodevcontato@gmail.com>
2024-03-15 00:34:14 -0300

    feat: created logic to verify token exitis of db

--- src/repositories/tokenFCM.ts
+++ src/repositories/tokenFCM.ts
@@ -3,11 +3,15 @@ import { Token } from '../types/tokenFCM';
 
 export class TokenFcmRepository {
 	public async create(token: string): Promise<Token> {
-		return new Promise((resolve, reject) => {
-			const modelTokensFCM = new modelTokenFCM({ token });
-			modelTokensFCM.save()
-				.then((token) => resolve(token))
-				.catch(() => reject(false));
+		// eslint-disable-next-line no-async-promise-executor
+		return new Promise( async (resolve, reject) => {
+			const tokenExists = await modelTokenFCM.findOne({ token: token });
+			if (!tokenExists) {
+				const modelTokensFCM = new modelTokenFCM({ token });
+				modelTokensFCM.save()
+					.then((token) => resolve(token))
+					.catch(() => reject(false));
+			}
 		});
 	}
 	public async readAll(): Promise<Token[]> {
